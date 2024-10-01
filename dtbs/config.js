21d00dea7196172c3fe43099ebf5340a60049b3a
Bazengmandweke <ndunguedu21@gmail.com>
2024-10-01 13:12:55 +0300

    Update config.js

--- dtbs/config.js
+++ dtbs/config.js
@@ -21,9 +21,9 @@ global.autoviewstatus = process.env.autoviewstatus || "TRUE"
 global.linkgc = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e'
 global.delayjpm = 3500
 //Panel
-global.domain = '' // your admin panel domian
-global.apikey = '' // Your admin panel apikey
-global.capikey = '' // Your admin panel capikey
+global.domain = 'https://pripatpanel.fajar.digital-server.biz.id/' // your admin panel domian
+global.apikey = 'ptlc_1juMWVqoIjg5eBhFH9eaZTe8jXJphmyS1rBdct1LMQT' // Your admin panel apikey
+global.capikey = 'ptla_OvbPs5IKZor2uaGlVMAjoIymgq71aI4PDb6kKhR2lho' // Your admin panel capikey
 global.egg = '15' // 
 global.location = '1' // 
 global.thumbnailpanel = 'https://i.imgur.com/N1zWDRi.jpeg'
